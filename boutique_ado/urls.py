9d1f8ae463819783e46ab960f0a1dcc543819174
coderlawa <5d12tpff@students.codeinstitute.net>
2026-08-15 20:33:53 +0100

    added shopping bag app, urls & template

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),  # Add this line for django-allauth
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
