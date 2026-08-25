560d9ff630bbe665e9c6dfa04fba59a70b24c76c
coderlawa <5d12tpff@students.codeinstitute.net>
2026-08-25 21:24:13 +0100

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NNvZBAPy9Ym6myIaxoLj4xpa2znWq0W5GYo6HYUmg6OTA1sBuePmmIXa0otppKbPiHKyq1M5b5IawdtmPhdsQlK00Y1WWPTVf',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
