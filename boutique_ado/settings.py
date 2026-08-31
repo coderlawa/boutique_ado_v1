479ef0cbde15a1ff4fd6db325c68caf0c000624b
coderlawa <5d12tpff@students.codeinstitute.net>
2026-08-31 19:47:50 +0100

    Stripe webhook connections added, tested & confirmed

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = 'django-insecure-4suh9ixpdb_uk8=bjr3*!^pb*if+v%y^#_ro@311cg)=_txty^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [
+    '127.0.0.1',    # vs code live server
+    'localhost',    # listen for stripes webhooks
+    ]
 
 
 # Application definition
@@ -176,6 +179,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
